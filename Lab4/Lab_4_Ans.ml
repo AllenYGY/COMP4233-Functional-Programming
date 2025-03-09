b38c8ddc3426f679445b33be572a59963393ec7c
AllenYGY <allenygy@outlook.com>
2025-03-09 21:19:21 +0800

    FP-Lab-4

--- Lab4/Lab_4_Ans.ml
+++ Lab4/Lab_4_Ans.ml
@@ -7,20 +7,20 @@ let score_to_grade (s:int) : grade =
     else if s>=20 then D
       else F;;
 
-type gd_rcd = {
-  name : string ,
-  score :int ,
+type gd_rcd ={
+  name : string;
+  score :int;
   lt_gd : grade;
 };;
 
 type gd_sheet ={
-    course_code:string,
-    instructor:string,
+    course_code:string;
+    instructor:string;
     content : gd_rcd list;
 };;
 
 let rec grade_check (gd_rcd_list:gd_rcd list) = 
   match gd_rcd_list with
   |[]-> false
+  |[x] -> x.lt_gd == score_to_grade x.score
   |hd::tl -> (hd.lt_gd == score_to_grade hd.score) && grade_check tl;;
-
