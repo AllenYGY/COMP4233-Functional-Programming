eec8d833221ec40038765e9e6dd011d97087d3a6
AllenYGY <allenygy@outlook.com>
2025-03-13 15:01:58 +0800

    Lab4

--- Lab4/Lab_4_Ans.ml
+++ Lab4/Lab_4_Ans.ml
@@ -21,6 +21,6 @@ type gd_sheet ={
 
 let rec grade_check (gd_rcd_list:gd_rcd list) = 
   match gd_rcd_list with
-  |[]-> false
+  |[]-> true
   |[x] -> x.lt_gd == score_to_grade x.score
   |hd::tl -> (hd.lt_gd == score_to_grade hd.score) && grade_check tl;;
