e6d114a3e85ef1fe78e29bad53a8cce496afef7d
AllenYGY <allenygy@outlook.com>
2025-06-06 23:22:16 +0800

    FP PA

--- PA/src/main.ml
+++ PA/src/main.ml
@@ -18,7 +18,7 @@ let step_binop = function
   | (Mult, Int a, Int b) -> Int (a * b)
   | (Leq, Int a, Int b) -> Bool (a <= b)
   | (And, Bool a, Bool b) -> Bool (a && b)
-  | (Cons, v, List) ->  Binop(Cons, v, List)
+  | (Cons, v1, v2) -> Binop(Cons, v1, v2)
   | _ -> failwith "Invalid binary operation"
 
 let rec substitute n v e =
@@ -78,15 +78,15 @@ let rec string_of_val = function
   | Int i -> string_of_int i
   | Bool b -> string_of_bool b
   | List -> "[]"
-  | Fun (name, _) -> "fun " ^ name ^ " -> ..."
+  | Fun (name, _) -> "fun " ^ name ^ " -> ..." 
   | Binop(Cons, h, t) -> 
       let rec list_elements = function
         | List -> ""
         | Binop(Cons, h, List) -> string_of_val h
-        | Binop(Cons, h, t) -> string_of_val h ^ "; " ^ list_elements t
+        | Binop(Cons, h, t) -> string_of_val h ^ " :: " ^ list_elements t
         | _ -> failwith "Not a valid list"
       in
-      "[" ^ list_elements (Binop(Cons, h, t)) ^ "]"
+      list_elements (Binop(Cons, h, t)) ^ " :: []"
   | _ -> failwith "Not a value"
 
 let rec string_of_expr = function
@@ -107,6 +107,4 @@ let rec eval e =
 let interp s = 
   s |> parse |> eval |> string_of_val
 
-let () =
-  let s = read_line () in
-  print_endline (interp s)
+(* let () = let s = read_line () in print_endline (interp s) *)
--- PA/test/test.ml
+++ PA/test/test.ml
@@ -18,6 +18,9 @@ let evalu_b name i s =
 let evalu_list name expected s =
   name >:: (fun _ -> assert_equal expected (interp s))
 
+let evalu_s name expected s =
+  name >:: (fun _ -> assert_equal expected (interp s))
+
 let tests = [
   parse_i "Parse1 int 22" (Int 22) "22";
   parse_i "Parse2 int -1" (Int (-1)) "-1";
@@ -31,7 +34,69 @@ let tests = [
   evalu_i "Evalu4 int 2*3+5" (11) "2*3+5";
   evalu_i "Evalu5 let x = 1 in x" (1) "let x = 1 in x";
   evalu_b "Evalu6 let x = (fun a -> (if (a<=1) then true else false)) in (x 1)" (true) "let x = (fun a -> (if (a<=1) then true else false)) in (x 1)";
-  evalu_list "List test" "[true]" "let x = (fun a -> (if (a<=1) then true else false)) in ((x 1)::[])";
+  
+  evalu_list "List test" "true :: []" "let x = (fun a -> (if (a<=1) then true else false)) in ((x 1)::[])";
+
+  parse_i "Parse7 bool true" (Bool true) "true";
+  parse_i "Parse8 bool false" (Bool false) "false";
+  parse_i "Parse9 empty list" (List) "[]";
+  parse_i "Parse10 cons" (Binop (Cons, Int 1, List)) "1::[]";
+  parse_i "Parse11 leq" (Binop (Leq, Int 1, Int 2)) "1<=2";
+  parse_i "Parse12 and" (Binop (And, Bool true, Bool false)) "true^false";
+  parse_i "Parse13 fun" (Fun ("x", Id "x")) "fun x -> x";
+  parse_i "Parse14 app" (App (Id "f", Int 1)) "f 1";
+  parse_i "Parse15 if" (If (Bool true, Int 1, Int 2)) "if true then 1 else 2";
+  parse_i "Parse16 nested let" (Let ("x", Int 1, Let ("y", Int 2, Binop (Add, Id "x", Id "y")))) "let x = 1 in let y = 2 in x + y";
+
+  evalu_i "Arith1 zero" 0 "0";
+  evalu_i "Arith2 addition" 7 "3+4";
+  evalu_i "Arith3 multiplication" 15 "3*5";
+  evalu_i "Arith4 complex" 14 "2+3*4";
+  evalu_i "Arith5 parentheses" 20 "(2+3)*4";
+  evalu_i "Arith6 negative" (-5) "-5";
+  evalu_i "Arith7 negative operation" (-2) "-5+3";
+
+  evalu_b "Bool1 true" true "true";
+  evalu_b "Bool2 false" false "false";
+  evalu_b "Bool3 and true" true "true^true";
+  evalu_b "Bool4 and false" false "true^false";
+  evalu_b "Bool5 and false false" false "false^false";
+
+  evalu_b "Comp1 leq true" true "3<=5";
+  evalu_b "Comp2 leq false" false "5<=3";
+  evalu_b "Comp3 leq equal" true "3<=3";
+  evalu_b "Comp4 complex comp" true "(2+3)<=(4*2)";
+
+  evalu_i "Let1 simple" 5 "let x = 5 in x";
+  evalu_i "Let2 addition" 8 "let x = 3 in x + 5";
+  evalu_i "Let3 nested" 10 "let x = 3 in let y = 7 in x + y";
+  evalu_i "Let4 shadowing" 7 "let x = 3 in let x = 7 in x";
+  evalu_i "Let5 complex" 15 "let x = 2 in let y = x + 3 in x * y + 5";
+
+  evalu_s "Fun1 identity" "fun x -> ..." "fun x -> x";
+  evalu_i "Fun2 app simple" 5 "(fun x -> x) 5";
+  evalu_i "Fun3 app add" 8 "(fun x -> x + 3) 5";
+  evalu_i "Fun4 nested fun" 10 "(fun x -> (fun y -> x + y) 3) 7";
+  evalu_b "Fun5 bool fun" false "(fun x -> x^true) false";
+
+  evalu_i "If1 true branch" 1 "if true then 1 else 2";
+  evalu_i "If2 false branch" 2 "if false then 1 else 2";
+  evalu_i "If3 complex guard" 10 "if 3<=5 then 10 else 20";
+  evalu_i "If4 nested if" 1 "if true then (if false then 2 else 1) else 3";
+
+  evalu_s "List1 empty" "[]" "[]";
+  evalu_s "List2 single int" "5 :: []" "5::[]";
+  evalu_s "List3 single bool" "true :: []" "true::[]";
+  evalu_s "List4 multiple ints" "1 :: 2 :: 3 :: []" "1::2::3::[]";
+  evalu_s "List5 multiple bools" "true :: false :: []" "true::false::[]";
+  evalu_s "List6 mixed with let" "5 :: []" "let x = 5 in x::[]";
+
+  evalu_i "Complex1 conditional function" 1 "let check = (fun n -> if n<=1 then 1 else n+10) in check 0";
+  evalu_i "Complex2 let with fun" 25 "let square = (fun x -> x*x) in square 5";
+  evalu_i "Edge1 zero mult" 0 "0*100";
+  evalu_i "Edge2 zero add" 5 "0+5";
+  evalu_b "Edge3 equal leq" true "5<=5";
+
 ]
 
 let _ = run_test_tt_main ("suite" >::: tests)
